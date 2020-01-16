f750e78c49dc23617391de8508ca9504b4dea514
Louis <32972874+gLevaa@users.noreply.github.com>
2020-01-16 21:16:08 +1300

    Update check.php

--- moonlight/check.php
+++ moonlight/check.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if(!isset($_SESSION['username']) && !isset($_SESSION['password']) && $_SESSION['suwoo'] != 'suwoo')
+if(!isset($_SESSION['username']) || !isset($_SESSION['password']) || $_SESSION['suwoo'] != 'suwoo')
 {
     header("Location: https://antrax.pw/indica/");
 }
